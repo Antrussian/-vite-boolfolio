c7a909f3520094547aa4e95236157df9784eb63e
Antrussian <antonino.russo.agent@gmail.com>
2024-02-20 23:33:58 +0100

    test sass

--- boolfolio-front-end/src/App.vue
+++ boolfolio-front-end/src/App.vue
@@ -1,12 +1,15 @@
 <script>
-//import HelloWorld from './components/HelloWorld.vue'
+
 
 </script>
 
 <template>
-  
+  <h1>
+    ciaoo
+  </h1>
 </template>
 
-<style>
+<style lang="scss">
+@use './style/general.scss' as *;
 
 </style>
